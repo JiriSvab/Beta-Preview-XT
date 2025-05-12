47cb40405501dcbffcbc8d99613445ba70dff492
Jirka <102830924+JiriSvab@users.noreply.github.com>
2025-05-12 13:24:44 +0200

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,12 @@
-document.getElementById('beta_prev_button').addEventListener('click', function() {
+document.getElementById('prev_button').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
         chrome.tabs.sendMessage(activeTabs[0].id, { action: 'executeCode' });
     });
 });
+
+document.getElementById('live_button').addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+        chrome.tabs.sendMessage(activeTabs[0].id, { action: 'executeLiveLinkCode' });
+    });
+});
+
