1575563bc6981b8e8d7110fcfa255f7ba4fa19ee
JiriSvab <svab.jiri@gmail.com>
2026-05-29 14:22:35 +0200

    Button Staging in Lazio - functional code

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,19 @@
-document.getElementById('staging_button').addEventListener('click', function() {
-    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-        chrome.tabs.sendMessage(activeTabs[0].id, { action: 'executeStagingLinkCode' });
+function sendToActiveTab(action) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(activeTabs) {
+        chrome.tabs.sendMessage(activeTabs[0].id, { action: action }, function() {
+            if (chrome.runtime.lastError) { /* no content script on this tab */ }
+        });
     });
+}
+
+document.getElementById('staging_button').addEventListener('click', function() {
+    sendToActiveTab('executeStagingLinkCode');
 });
 
 document.getElementById('prev_button').addEventListener('click', function() {
-    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-        chrome.tabs.sendMessage(activeTabs[0].id, { action: 'createPreviewLink' });
-    });
+    sendToActiveTab('createPreviewLink');
 });
 
 document.getElementById('live_button').addEventListener('click', function() {
-    chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-        chrome.tabs.sendMessage(activeTabs[0].id, { action: 'executeLiveLinkCode' });
-    });
+    sendToActiveTab('executeLiveLinkCode');
 });
-
