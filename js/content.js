314ae6cc360d969db2db707ce7720c4adf23fb31
JiriSvab <svab.jiri@gmail.com>
2026-05-29 14:17:08 +0200

    Button Staging in Lazio colours - shows bug in popup.html

--- js/content.js
+++ js/content.js
@@ -38,6 +38,45 @@ chrome.runtime.onMessage.addListener(function(request) {
         createLink();
     }
 
+    if(request.action === 'executeStagingLinkCode') {
+        // Creation of Staging link
+        var item_path = document.querySelector(".scEditorPanel > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td:nth-child(2) > input").value;
+        var regex_leica = /sitecore\/content\/hexagon\/hexagon-geosystems-brand\/hexagon-geosystems/;
+        var regex_heavy = /sitecore\/content\/hexagon\/hexagon-geosystems-brand\/heavy-construction/;
+        var regex_3DSurveillance = /sitecore\/content\/hexagon\/hexagon-geosystems-brand\/3DSurveillance/;
+        var regex_geomax = /sitecore\/content\/hexagon\/hexagon-geosystems-brand\/hexagon-geomax\/home/;
+        var regex_hxgncontent = /sitecore\/content\/hexagon\/hexagon-geosystems-brand\/hxgn-content-program\/home/;
+        var regex_hxgnsmartnet = /sitecore\/content\/hexagon\/hexagon-geosystems-brand\/smartnet\/home/;
+        var regex_idsgeoradar = /sitecore\/content\/hexagon\/hexagon-geosystems-brand\/ids-georadar\/home/;
+        var regex_OneWeb_master = /sitecore\/content\/One Web\/Master Site\/Home/;
+
+        if (regex_leica.test(item_path) == true) {
+            var staging_button = item_path.replace("/sitecore/content/hexagon/hexagon-geosystems-brand/hexagon-geosystems/home","https://staging.leica-geosystems.com");
+        } else if (regex_heavy.test(item_path) == true) {
+            var staging_button = item_path.replace("/sitecore/content/hexagon/hexagon-geosystems-brand/heavy-construction/Home","https://staging-heavyconstruction.hexagon.com");
+        } else if (regex_3DSurveillance.test(item_path) == true){
+            var staging_button = item_path.replace("/sitecore/content/hexagon/hexagon-geosystems-brand/3DSurveillance/Home","https://staging-3dsurveillance.hexagon.com");
+        } else if (regex_geomax.test(item_path) == true) {
+            var staging_button = item_path.replace("/sitecore/content/hexagon/hexagon-geosystems-brand/hexagon-geomax/home","https://staging.geomax-positioning.com");
+        } else if (regex_hxgncontent.test(item_path) == true) {
+            var staging_button = item_path.replace("/sitecore/content/hexagon/hexagon-geosystems-brand/hxgn-content-program/home","https://staging.hxgncontent.com");
+        } else if (regex_hxgnsmartnet.test(item_path) == true) {
+            var staging_button = item_path.replace("/sitecore/content/hexagon/hexagon-geosystems-brand/smartnet/home","https://staging.hxgnsmartnet.com");
+        } else if (regex_idsgeoradar.test(item_path) == true) {
+            var staging_button = item_path.replace("/sitecore/content/hexagon/hexagon-geosystems-brand/ids-georadar/home","https://staging.idsgeoradar.com");
+        } else if (regex_OneWeb_master.test(item_path) == true) {
+            var staging_button = item_path.replace("/sitecore/content/One Web/Master Site/Home","https://staging.hexagon.com");
+        }  else {
+            var staging_button = item_path.innerHTML = "I dont know Staging link of this";
+        }
+
+        function createLink() {
+            var item_ID_row = document.querySelector(".scEditorPanel > table > tbody > tr > td > table > tbody > tr:nth-child(2) ");
+            item_ID_row.insertAdjacentHTML('beforebegin', '<tr><td>Staging: </td><td><a href="'+staging_button+'" target="_blank">'+staging_button+'</a></td></tr>');
+        };
+        createLink();
+    }
+
     if(request.action === 'executeLiveLinkCode') {
         // Creation of Live link
         var item_path = document.querySelector(".scEditorPanel > table > tbody > tr > td > table > tbody > tr:nth-child(3) > td:nth-child(2) > input").value;
